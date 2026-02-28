761686ccbedb448de1737b8c9907b7a9ffe07cad
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-02-28 10:55:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div style={{ maxWidth: 720, margin: "40px auto", fontFamily: "system-ui" }}>
       <h1>ARCHIMEDES Live</h1>
-      <p>První deploy portálu – Next.js běží.</p>
-    </div>
+      <p>První deploy portálu – Next.js běží ✅</p>    </div>
   );
 }
