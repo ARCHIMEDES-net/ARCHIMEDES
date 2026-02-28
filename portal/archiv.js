6a280b0992702084f7cf4fd586fc80c92ba2f1c3
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-02-28 11:34:08 +0100

    Delete portal/archiv.js

--- portal/archiv.js
+++
@@ -1,9 +0,0 @@
-export default function archiv() {
-  return (
-    <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
-      <h1>Kalendář</h1>
-      <p>Zde bude kalendář událostí + odkazy na vysílání.</p>
-      <p><a href="/portal">← Zpět do portálu</a></p>
-    </div>
-  );
-}
