386dda80264a6adbe4f2bede21a22b57966dc9a9
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-02-28 13:08:50 +0100

    Update index.js

--- pages/portal/index.js
+++ pages/portal/index.js
@@ -1,19 +1,21 @@
+import Link from "next/link";
+
 export default function PortalHome() {
   return (
     <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
       <h1>Portál – registrovaná část</h1>
 
       <ul>
-        <li><a href="/portal/kalendar">Kalendář</a></li>
-        <li><a href="/portal/program">Program</a></li>
-        <li><a href="/portal/archiv">Archiv</a></li>
-        <li><a href="/portal/pracovni-listy">Pracovní listy</a></li>
-        <li><a href="/portal/inzerce">Inzerce</a></li>
-        <li><a href="/portal/clenove">Databáze členů</a></li>
+        <li><Link href="/portal/kalendar">Kalendář</Link></li>
+        <li><Link href="/portal/program">Program</Link></li>
+        <li><Link href="/portal/archiv">Archiv</Link></li>
+        <li><Link href="/portal/pracovni-listy">Pracovní listy</Link></li>
+        <li><Link href="/portal/inzerce">Inzerce</Link></li>
+        <li><Link href="/portal/clenove">Databáze členů</Link></li>
       </ul>
 
       <p style={{ marginTop: 18 }}>
-        <a href="/">← Zpět na veřejnou část</a>
+        <Link href="/">Zpět na veřejnou část</Link>
       </p>
     </div>
   );
