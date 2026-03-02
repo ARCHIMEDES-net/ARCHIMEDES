98306340013ace43ed6111cb3b6a903c14c14fa8
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-02 12:38:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,5 @@
-import { useRouter } from "next/router";
-import RequireAuth from "../components/RequireAuth";
+import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  // pokud URL začíná /portal → chránit
-  if (router.pathname.startsWith("/portal")) {
-    return (
-      <RequireAuth>
-        <Component {...pageProps} />
-      </RequireAuth>
-    );
-  }
-
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
