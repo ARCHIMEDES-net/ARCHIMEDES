911b9858bf088f043204c2073b659f98665b05aa
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-04 21:51:16 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+// pages/_app.js
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import PublicHeader from "../components/PublicHeader";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const path = router.pathname || "";
+
+  const isPortal = path.startsWith("/portal");
+  const isAuthPage = path === "/login"; // případně i /logout, pokud ho používáte jako stránku
+  const showPublicHeader = !isPortal && !isAuthPage;
+
+  return (
+    <>
+      {showPublicHeader && <PublicHeader />}
+      <Component {...pageProps} />
+    </>
+  );
 }
