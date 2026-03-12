b401f6dcd472e237d73a170c27c9e9bd918f097a
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-12 18:38:28 +0100

    Update instagram.js

--- pages/api/instagram.js
+++ pages/api/instagram.js
@@ -1,13 +1,14 @@
-const posts = [
-  { type: "p", id: "SEM_DEJ_NEJNOVĚJŠÍ_ID_1" },
-  { type: "p", id: "SEM_DEJ_NEJNOVĚJŠÍ_ID_2" },
-  { type: "reel", id: "SEM_DEJ_NEJNOVĚJŠÍ_ID_3" },
-];
-
 export default async function handler(req, res) {
+  const posts = [
+    { type: "p", id: "DVyqPmiiLKF" },
+    { type: "p", id: "DVvUBXDCMYC" },
+    { type: "p", id: "DVqEttcjpu0" }
+  ];
+
   res.setHeader(
     "Cache-Control",
     "no-store, no-cache, must-revalidate, max-age=0"
   );
+
   return res.status(200).json(posts);
 }
