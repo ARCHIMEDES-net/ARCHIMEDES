b516f12652784e947fda3fb977ea6cd612d033b5
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-21 19:31:52 +0100

    Update program.js

--- pages/portal/program.js
+++ pages/portal/program.js
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import RequireAuth from "../../components/RequireAuth";
+import PortalHeader from "../../components/PortalHeader";
 
 export default function Program() {
   return (
-    <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
-      <h1>Program</h1>
-      <p>Zde bude přehled programů a rubrik.</p>
-      <p><Link href="/portal">← Zpět do portálu</Link></p>
-    </div>
+    <RequireAuth>
+      <PortalHeader />
+
+      <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
+        <h1>Program</h1>
+        <p>Zde bude přehled programů a rubrik.</p>
+
+        <p>
+          <Link href="/portal">← Zpět do portálu</Link>
+        </p>
+      </div>
+    </RequireAuth>
   );
 }
