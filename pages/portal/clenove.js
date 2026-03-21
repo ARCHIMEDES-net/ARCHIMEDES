01924892c2974ae5935cf4fa464a9e0676ee4af7
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-21 19:33:05 +0100

    Update clenove.js

--- pages/portal/clenove.js
+++ pages/portal/clenove.js
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import RequireAuth from "../../components/RequireAuth";
+import PortalHeader from "../../components/PortalHeader";
 
 export default function Clenove() {
   return (
-    <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
-      <h1>Databáze členů</h1>
-      <p>Zde bude seznam obcí/škol a správa účtů.</p>
-      <p><Link href="/portal">← Zpět do portálu</Link></p>
-    </div>
+    <RequireAuth>
+      <PortalHeader />
+
+      <div style={{ maxWidth: 900, margin: "40px auto", fontFamily: "system-ui", padding: 16 }}>
+        <h1>Databáze členů</h1>
+        <p>Zde bude seznam obcí/škol a správa účtů.</p>
+
+        <p>
+          <Link href="/portal">← Zpět do portálu</Link>
+        </p>
+      </div>
+    </RequireAuth>
   );
 }
