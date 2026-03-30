1ad15ec19bf359222d2c8d09591ec7919a9e8fb4
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-03-30 11:29:22 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
                     Ukázková hodina
                   </ButtonLink>
                   <ButtonLink href="/demo" variant="secondary">
-                    Chci DEMO
+                    DEMO platformy
                   </ButtonLink>
                   <ButtonLink href="/start" variant="secondary">
                     Balíček START
