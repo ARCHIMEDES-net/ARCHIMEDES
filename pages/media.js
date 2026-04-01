bcf7a3ecac8977f40133f93253c54ea7db973506
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-04-01 11:34:02 +0200

    Update media.js

--- pages/media.js
+++ pages/media.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { supabase } from "../lib/supabaseClient";
 
-const heroImg = "/ucebna-exterier.webp";
+const heroImg = "/kamera.webp";
 const classImg = "/ucebna-deti.webp";
 const techImg = "/ucebna-technologie.webp";
 const communityImg = "/ucebna-komunita.webp";
