fd7a3484cad4eb51281f392e9c84d326878772e9
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-07-16 19:44:26 +0200

    Přesměrovat ukončený ARCHIMEDES DAY

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
       { source: "/vysilani", destination: "/program#archiv", permanent: true },
       { source: "/aktualni-pozvanky", destination: "/program#vysilani", permanent: true },
       { source: "/reference", destination: "/ucebna#oceneni", permanent: true },
+      { source: "/archimedes-day", destination: "/program", permanent: true },
       { source: "/portal/program", destination: "/portal/kalendar", permanent: true },
       { source: "/portal/admin-udalosti", destination: "/portal/admin/udalosti", permanent: true },
     ];
