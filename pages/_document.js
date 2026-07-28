870df1b190cf5e4abb12fff7cdc5d8a4c9871a41
ARCHIMEDES-net <antonin.koplik@gmail.com>
2026-07-28 09:54:29 +0200

    Nasadit Google Tag Manager GTM-PSMPHVW5

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,47 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const GTM_ID = "GTM-PSMPHVW5";
+
 export default function Document() {
   return (
     <Html lang="cs">
-      <Head />
+      <Head>
+        <script
+          id="google-consent-default-document"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+window.gtag = window.gtag || gtag;
+gtag('consent', 'default', {
+  analytics_storage: 'denied',
+  ad_storage: 'denied',
+  ad_user_data: 'denied',
+  ad_personalization: 'denied',
+  wait_for_update: 500
+});`,
+          }}
+        />
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+      </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
