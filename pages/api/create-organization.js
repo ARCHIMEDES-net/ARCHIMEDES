39ad55d7ccc022a3d323be2115660b165e88d326
Antonín Koplík <antonin.koplik@gmail.com>
2026-07-29 17:18:44 +0200

    security: disable caching for retired APIs

--- pages/api/create-organization.js
+++ pages/api/create-organization.js
@@ -1,4 +1,5 @@
 export default function handler(_req, res) {
+  res.setHeader("Cache-Control", "no-store");
   return res.status(410).json({
     error:
       "Organizace vzniká až po ověřené žádosti. Obec, škola i spolek mohou požádat o samostatné zapojení; škola a spolek se mohou také registrovat pod aktivní obcí.",
